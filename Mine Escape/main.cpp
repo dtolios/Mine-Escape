fd87ce54b7a8daa45663d4700e583ced9ed84657
Dimitri Tolios <dtolios@riotgames.com>
2016-10-26 15:05:34 -0700

    Initial Commit

---
+++ Mine Escape/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mine Escape
+//
+//  Created by Dimitri Tolios on 10/26/16.
+//  Copyright © 2016 Dimitri Tolios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
